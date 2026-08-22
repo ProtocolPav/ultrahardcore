f5cef5012ee0075f918e15631b6ec9206c2354d0
Pavel Krutov <pavel@krutov.eu>
2026-08-22 10:56:42 +0100

    feat: add support for checking equipped items in player inventory

--- behaviour_pack/scripts-dev/utils/check_player_has_item.ts
+++ behaviour_pack/scripts-dev/utils/check_player_has_item.ts
@@ -1,7 +1,9 @@
-import {EntityComponentTypes, ItemType, Player} from "@minecraft/server";
+import {EntityComponentTypes, EquipmentSlot, ItemType, Player} from "@minecraft/server";
 
 export default function player_has_item(player: Player, item_id: string) {
     const inventory = player.getComponent(EntityComponentTypes.Inventory)
+    const armour = player.getComponent(EntityComponentTypes.Equippable)
+
     if (inventory?.container) {
         const inventory_size = inventory.inventorySize
         const inventory_container = inventory.container
@@ -12,6 +14,12 @@ export default function player_has_item(player: Player, item_id: string) {
             }
         }
     }
+
+    if (armour?.getEquipment(EquipmentSlot.Feet)?.typeId === item_id) return true
+    else if (armour?.getEquipment(EquipmentSlot.Legs)?.typeId === item_id) return true
+    else if (armour?.getEquipment(EquipmentSlot.Chest)?.typeId === item_id) return true
+    else if (armour?.getEquipment(EquipmentSlot.Head)?.typeId === item_id) return true
+    else if (armour?.getEquipment(EquipmentSlot.Offhand)?.typeId === item_id) return true
 
     return false
 }
